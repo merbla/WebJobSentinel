af211e2f54ddb9943f57c9bd3653261187375a0a
Matthew Erbs <matthew.erbs@merbla.com>
2015-02-14 09:22:12 +1100

    Added simple implementation

---
+++ src/WebJobSentinel.Example/Program.cs
@@ -0,0 +1,46 @@
+﻿#region
+
+using System;
+using System.Threading;
+using Serilog;
+
+#endregion
+
+namespace WebJobSentinel.Example
+{
+    namespace WebJobSentinel.Example
+    {
+        internal class Program :IDisposable
+
+        {
+            private static bool _running = true;
+            private static Sentinel _sentinel;
+
+            private static void Main()
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.ColoredConsole()
+                    .CreateLogger();
+
+
+                _sentinel = new Sentinel(args => Log.Information("File Created"),  args => Log.Information("File Changed"));
+
+
+                // Run as long as we didn't get a shutdown notification
+                while (_running)
+                {
+                    // Here is my actual work
+                    Log.Information("Running and waiting " + DateTime.UtcNow);
+                    Thread.Sleep(1000);
+                }
+
+                Log.Information("Stopped " + DateTime.UtcNow);
+            }
+
+            public void Dispose()
+            {
+                _sentinel.Dispose();
+            }
+        }
+    }
+}
---
+++ src/WebJobSentinel/Sentinel.cs
@@ -0,0 +1,44 @@
+﻿#region
+
+using System;
+using System.IO;
+
+#endregion
+
+namespace WebJobSentinel
+{
+    public class Sentinel : IDisposable
+    {
+        private readonly Action<string> _log;
+        public const string WebjobsShutdownFile = "WEBJOBS_SHUTDOWN_FILE";
+        private readonly FileSystemWatcher _shutdownFileSystemWatcher;
+
+
+        public Sentinel()
+        {
+            string shutdownFile = Environment.GetEnvironmentVariable(WebjobsShutdownFile);
+
+            _shutdownFileSystemWatcher = new FileSystemWatcher(Path.GetDirectoryName(shutdownFile));
+            _shutdownFileSystemWatcher.Created += (sender, args) => OnShutdownFileCreated(args);
+            _shutdownFileSystemWatcher.Changed += (sender, args) => OnShutdownFileChanged(args);
+            _shutdownFileSystemWatcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.LastWrite;
+            _shutdownFileSystemWatcher.IncludeSubdirectories = false;
+            _shutdownFileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        public Sentinel(Action<FileSystemEventArgs> onShutdownFileCreated,
+            Action<FileSystemEventArgs> onShutdownFileChanged) : this()
+        {
+            OnShutdownFileCreated = onShutdownFileCreated;
+            OnShutdownFileChanged = onShutdownFileChanged;
+        }
+
+        public Action<FileSystemEventArgs> OnShutdownFileCreated { get; set; }
+        public Action<FileSystemEventArgs> OnShutdownFileChanged { get; set; }
+
+        public void Dispose()
+        {
+            _shutdownFileSystemWatcher.Dispose();
+        }
+    }
+}
